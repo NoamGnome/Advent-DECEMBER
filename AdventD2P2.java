e539e00f365a82f6009c453e977d0e618cc622ec
NoamGnome <146112021+NoamGnome@users.noreply.github.com>
2024-12-18 14:47:52 -0500

    Add files via upload

--- AdventD2P2.java
+++ AdventD2P2.java
@@ -1,30 +1,55 @@
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class AdventD2P2 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/Day2Input.txt");
-        ArrayList<String> correctness = new ArrayList<>();
+        int total = 1000;
+        ArrayList<String> wrongs = new ArrayList<>();
         for (int i = 0; i < fileData.size(); i++) {
-            String[] integers = fileData.get(i).split(" ");
+            boolean increasing = true;
+            boolean decreasing = true;
+            boolean match = true;
+            for (int j = 1; j < fileData.get(i).split(" ").length; j++) {
+                int integer = Integer.parseInt(fileData.get(i).split(" ")[j]);
+                int integer1 = Integer.parseInt(fileData.get(i).split(" ")[j - 1]);
+                if (Math.abs(integer - integer1) < 1 || Math.abs(integer - integer1) > 3) {
+                    match = false;
+                }
+                if (integer < integer1) {
+                    increasing = false;
+                }
+                if (integer > integer1) {
+                    decreasing = false;
+                }
+                if (!increasing && !decreasing) {
+                    match = false;
+                }
+            }
+            if (!match) {
+                if (!wrongs.contains(fileData.get(i))) {
+                    wrongs.add(fileData.get(i));
+                }
+            }
+        }
+        System.out.println(wrongs.size());
+        ArrayList<String> correctness = new ArrayList<>();
+        for (int i = 0; i < wrongs.size(); i++) {
+            String[] integers = wrongs.get(i).split(" ");
             ArrayList<Integer> news = new ArrayList<>();
             for (String s : integers) {
                 news.add(Integer.parseInt(s));
             }
-            System.out.println(news);
             for (int j = 0; j < integers.length; j++) {
                 boolean increasing = true;
                 boolean decreasing = true;
                 boolean match = true;
                 ArrayList<Integer> news1 = new ArrayList<>(news);
                 news1.remove(j);
-                System.out.println(news1);
                 for (int k = 1; k < news1.size(); k++) {
                     int integer = news1.get(k);
                     int integer1 = news1.get(k - 1);
@@ -40,10 +65,10 @@ public class AdventD2P2 {
                     if (!increasing && !decreasing) {
                         match = false;
                     }
-                    if (match) {
-                        if (!correctness.contains(fileData.get(i))) {
-                            correctness.add(fileData.get(i));
-                        }
+                }
+                if (match) {
+                    if (!correctness.contains(wrongs.get(i))) {
+                        correctness.add(wrongs.get(i));
                     }
                 }
             }
